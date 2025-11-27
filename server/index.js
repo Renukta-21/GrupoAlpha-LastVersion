be8d3d57d01bf36e6d4ce94211a90b06e18f134f
Renukta-21 <edu211004@gmail.com>
2025-11-27 08:59:34 -0600

    adding logger

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 
+const logger = require('./middleware/logger');
 const port = 3001 || process.env.PORT;
 
+app.use(logger);
 app.get('/', (req,res)=>{
-    console.log('Main route accessed');
     res.send('Hello from the server!');
+})
+
+app.get('/something', (req,res)=>{
+    res.send('Hello from something route!');
+})
+
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`);  
 })
