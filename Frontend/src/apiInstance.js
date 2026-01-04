984ec2ee93b68be7c4be83a863f7e19ff52d7e1e
Edward <edu211004@gmail.com>
2026-01-04 16:15:52 -0600

    adding instance

--- Frontend/src/apiInstance.js
+++ Frontend/src/apiInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const apiInstance = axios.create({
-    baseURL: "http://localhost:3001/"
+    baseURL: "https://grupoalpha-lastversion.onrender.com/"
 })
 
 export default apiInstance;
