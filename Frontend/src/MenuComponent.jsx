3dc63cead60fe4c8e003baae76569aeb96f5e14b
Edward <edu211004@gmail.com>
2026-01-04 15:35:35 -0600

    last updated

--- Frontend/src/MenuComponent.jsx
+++ Frontend/src/MenuComponent.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 import { IoSearch } from "react-icons/io5";
 import { MdNavigateNext } from "react-icons/md";
+import { IoCloseSharp } from "react-icons/io5";
+import SubMenuComponent from "./SubMenuComponent";
 
 function MenuComponent({ categories, menuOpened, setMenuOpened }) {
+    const categoryMockup = { id: '22', nombre: 'Videovigilancia', nivel: 1, subcategorias: [{id:1, nombre:'juasjuas'}]}
     const [selectedCategory, setSelectedCategory] = useState(null)
+
+    const handleOpenSubMenu = (e, category) => {
+        e.preventDefault()
+        setSelectedCategory(category)
+    }
+
     return (
         <>
             {menuOpened && (
@@ -12,40 +21,45 @@ function MenuComponent({ categories, menuOpened, setMenuOpened }) {
 
             <div
                 className={`${menuOpened
-                    ? "fixed z-20  bg-thirdBlue left-0 w-11/12 h-full translate-x-0 border-r-[1px] border-blue-950"
+                    ? "fixed z-20  bg-thirdBlue left-0 w-11/12 h-full sm:w-2/5 translate-x-0 border-r-[1px] border-blue-950"
                     : "hidden -translate-x-full"
                     }`}
             >
 
-                <section className="py-3 pl-4 pr-6 bg-secondaryBlue">
+                <section className="py-3 pl-4 bg-mainDarkBlue">
                     <div className="flex justify-between ">
                         <div className="border-[1px]   border-gray-500 rounded-xl flex items-center">
                             <IoSearch className="mx-4 text-gray-500" />
                             <input type="text" placeholder='Buscar categorias...' className='placeholder-gray-500 text-gray-300 outline-none py-2' />
                         </div>
-                        <div>
-                            <button className='text-gray-500 text-2xl' onClick={() => setMenuOpened(false)}>x</button>
+                        <div className="flex items-center">
+                            <button className='text-gray-500 text-2xl cursor-pointer mr-6' onClick={() => setMenuOpened(false)}>
+                                <IoCloseSharp/>
+                            </button>
                         </div>
                     </div>
                 </section>
-                <div className='w-full h-[1px] bg-blue-950'></div>
-                {selectedCategory ? <p>Hola</p> :
-                    <section className='h-full'>
-                        <ul className="flex flex-col gap-1 bg-thirdBlue mt-4 mr-3">
-                            {categories.map((category) => (
-                                <li key={category.id} className="text-gray-300 py-3   rounded-lg px-3 py-2 flex items-center justify-between">
-                                    <a href="" className='text-[14px] font-semibold'>{category.nombre}</a>
-                                    <a href="" className='text-xl'><MdNavigateNext /></a>
-                                </li>
-                            ))}
+                <div className="h-[1px] bg-gray-500 w-full"></div>
+
+                <div className='w-full h-[1px] px-2'></div>
+                {selectedCategory ? <SubMenuComponent category={selectedCategory} setSelectedCategory={setSelectedCategory}/> :
+                    <section className='h-full  flex flex-col bg-secondaryBlue'>
+                        <ul className="flex flex-col gap-1 mt-4">
+                            {categories.map((category) => (<DisplayedCategory key={category.id} category={category} handleOpenSubMenu={handleOpenSubMenu} />))}
                         </ul>
                     </section>}
             </div>
         </>
     )
 }
-function DisplayedCategory({selectedCategory}){
-    return()
+function DisplayedCategory({ category, handleOpenSubMenu }) {
+    return (
+        <div className="text-gray-300 py-3  hover:bg-blue-950 rounded-lg px-3 mx-4 flex items-center justify-between  cursor-pointer"
+            onClick={(e) => handleOpenSubMenu(e, category)}>
+            <a href="" className='text-[14px] font-semibold'>{category.nombre}</a>
+            <a href="" className='text-xl'><MdNavigateNext /></a>
+        </div>
+    )
 }
 
 export default MenuComponent
---
+++ Frontend/src/SubMenuComponent.jsx
@@ -0,0 +1,34 @@
+import { IoCloseSharp } from "react-icons/io5";
+import { IoChevronBack } from "react-icons/io5";
+import { MdNavigateNext } from "react-icons/md";
+
+function SubMenuComponent({ category, setSelectedCategory }) {
+    const {subcategorias} = category.subcategorias
+    return (
+        <div className="fixed inset-0 bg-secondaryBlue z-10">
+            <section className='px-4 py-4 flex justify-between bg-mainDarkBlue'>
+                <h2 className='text-white text-xl font-semibold'>{category.nombre}</h2>
+                <div className="flex items-center">
+                    <button className="text-white bg-buttonBlue flex rounded-2xl px-3 py-1 text-[13px] flex items-center">Ver todas <MdNavigateNext className="text-2xl font-bold" /></button>
+                    <span className="text-gray-500 text-xl mx-3"><IoCloseSharp /></span>
+                </div>
+            </section>
+            <div className="h-[1px] bg-gray-500 w-full"></div>
+            <section className="px-6 py-3  text-buttonBlue text-[16px] font-semibold">
+                <div className="flex items-center gap-2 hover:cursor-pointer hover:text-blue-300 px-3 max-w-fit" onClick={()=>setSelectedCategory(null)}><IoChevronBack />Volver</div>
+            </section>
+            <div className="h-[1px] bg-gray-500 w-full"></div>
+            <section className="mt-4">
+                {subcategorias.map(sub => (
+                    <div className="text-gray-300 px-3 ">
+                        <div className="flex items-center px-4 py-[11px] justify-between text-[15px] font-semibold hover:bg-blue-950 cursor-pointer rounded-md">
+                            <span>{sub.nombre}</span> <MdNavigateNext className="text-xl" />
+                        </div>
+                    </div>
+                ))}
+            </section>
+        </div>
+    )
+}
+
+export default SubMenuComponent
